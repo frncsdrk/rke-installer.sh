e3a76fe0e277ec04e7748f207db193005b78761f
frncsdrk <frncsdrk@protonmail.com>
2019-09-19 09:12:24 +0200

    Update rancher release version to 0.2.8

--- src/settings.sh
+++ src/settings.sh
@@ -8,5 +8,5 @@ DOWNLOAD_URL="https://github.com/frncsdrk/${INSTALLABLE_NAME}/archive/master.tar
 EXTRACTED_DIR_NAME="${INSTALLABLE_NAME}-master"
 
 RKE_DEST="/opt"
-RKE_DOWNLOAD_URL="https://github.com/rancher/rke/releases/download/v0.2.2/rke_linux-arm64"
+RKE_DOWNLOAD_URL="https://github.com/rancher/rke/releases/download/v0.2.8/rke_linux-arm64"
 RKE_BIN_TARGET="rke"
